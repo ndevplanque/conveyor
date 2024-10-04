29ed6813e1e5d12f77f35ca21202fe084fad885b
nicolasdelplanquechargemap <nicolas.delplanque@chargemap.com>
2024-10-04 13:32:48 +0200

    feat(Arduino): added AddStockMvtByRef()

--- Arduino/src/DolibarrFacade.cpp
+++ Arduino/src/DolibarrFacade.cpp
@@ -1,71 +1,87 @@
 #include "DolibarrFacade.h"
 
-DolibarrFacade::DolibarrFacade(String dolapiurl, String dolapikey)
-    : dolapiurl(dolapiurl), dolapikey(dolapikey) {
+DolibarrFacade::DolibarrFacade(String dolapiurl, String dolapikey, Logger *logs)
+    : dolapiurl(dolapiurl), dolapikey(dolapikey), logs(logs)
+{
 }
 
-HTTPClient* DolibarrFacade::dolibarr(String endpoint) {
-    HTTPClient* http = new HTTPClient(); 
+HTTPClient *DolibarrFacade::dolibarr(String endpoint)
+{
+    HTTPClient *http = new HTTPClient();
+
     String url = this->dolapiurl + endpoint;
-    Serial.println(url);
     http->begin(url);
+
+    http->setTimeout(30000);
     http->addHeader("Content-Type", "application/json");
     http->addHeader("DOLAPIKEY", this->dolapikey);
+
     return http;
 }
 
-ErrorCode DolibarrFacade::addStockMovementByRef(String productRef, int qty) {
-    if (WiFi.status() != WL_CONNECTED) {
-       return WIFI_NOT_CONNECTED;
+ErrorCode DolibarrFacade::addStockMovementByRef(String productRef, int qty)
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
+        return WIFI_NOT_CONNECTED;
     }
 
     JsonDocument productData;
-    ErrorCode code = getProductDataByRef(productRef, productData);
-    if (code != SUCCESS) {
-        return code;
+    ErrorCode error = getProductDataByRef(productRef, productData);
+    // logs->print("getProductDataByRef", error);
+    if (error != SUCCESS)
+    {
+        return error;
     }
 
     int productId = productData[0]["id"];
     int warehouseId = productData[0]["fk_default_warehouse"];
+    // logs->print("productId : " + String(productId) + " / warehouseId : " + String(warehouseId));
 
-    Serial.println("productId : " + String(productId) + " / warehouseId : " + String(warehouseId));
+    String endpoint = "/stockmovements";
+    String payload = "{\"product_id\":" + String(productId) + ",\"warehouse_id\":" + String(warehouseId) + ",\"qty\":" + String(qty) + "}";
+    HTTPClient *http = dolibarr(endpoint);
+    int httpCode = http->POST(payload);
+    logs->print(String(httpCode) + " POST " + endpoint);
 
-    HTTPClient* http = dolibarr("/stockmovements");
-    int httpCode = http->POST(
-        "{\"product_id\":" + String(productId) + ",\"warehouse_id\":" + String(warehouseId) + ",\"qty\":" + String(qty) + "}"
-    );
     http->end();
+    delete http;
 
-    Serial.println("addStockMovementByRef : " + String(httpCode));
-
-    if (httpCode < 200 || httpCode >= 300) {
+    if (httpCode < 200 || httpCode >= 300)
+    {
         return HTTP_REQUEST_FAILED;
     }
 
     return SUCCESS;
 }
 
-ErrorCode DolibarrFacade::getProductDataByRef(String productRef, JsonDocument &doc) {
-    if (WiFi.status() != WL_CONNECTED) {
+ErrorCode DolibarrFacade::getProductDataByRef(String productRef, JsonDocument &doc)
+{
+    if (WiFi.status() != WL_CONNECTED)
+    {
         return WIFI_NOT_CONNECTED;
     }
 
-    HTTPClient* http = dolibarr("/products?sqlfilters=t.ref:=:'" + productRef + "'&properties=id,fk_default_warehouse");
+    String endpoint = "/products?sqlfilters=t.ref:=:'" + productRef + "'&properties=id,fk_default_warehouse";
+    HTTPClient *http = dolibarr(endpoint);
     int httpCode = http->GET();
+    logs->print(String(httpCode) + " GET " + endpoint);
 
-    Serial.println("getProductDataByRef : " + String(httpCode));
-
-    if (httpCode < 200 || httpCode >= 300) {
+    if (httpCode < 200 || httpCode >= 300)
+    {
         http->end();
+        delete http;
         return HTTP_REQUEST_FAILED;
     }
 
     String payload = http->getString();
     http->end();
+    delete http;
 
     DeserializationError error = deserializeJson(doc, payload);
 
-    if (error) {
+    if (error)
+    {
         return JSON_PARSING_FAILED;
     }
 
--- Arduino/src/DolibarrFacade.h
+++ Arduino/src/DolibarrFacade.h
@@ -3,26 +3,23 @@
 
 #include <Arduino.h>
 #include <ArduinoJson.h>
-#include <HTTPClient.h>  
-#include <WiFi.h>      
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include "Logger.h"
+#include "ErrorCode.h"
 
-enum ErrorCode {
-    SUCCESS = 0,
-    WIFI_NOT_CONNECTED = 1,
-    HTTP_REQUEST_FAILED = 2,
-    JSON_PARSING_FAILED = 3
-};
-
-class DolibarrFacade {
+class DolibarrFacade
+{
 public:
-    DolibarrFacade(String dolapiurl, String dolapikey);
+    DolibarrFacade(String dolapiurl, String dolapikey, Logger *logs);
     ErrorCode addStockMovementByRef(String productRef, int qty);
     ErrorCode getProductDataByRef(String productRef, JsonDocument &doc);
 
 private:
     String dolapiurl;
     String dolapikey;
-    HTTPClient* dolibarr(String endpoint);
+    Logger *logs;
+    HTTPClient *dolibarr(String endpoint);
 };
 
 #endif
