a0d8a359c05eb357e1224a063ac5197685be578c
Nicolas <nicolas.bahl@epitech.eu>
2025-01-23 13:43:00 +0100

    chore: add schéma d'architecture

---
+++ Arduino/src/env.h
@@ -0,0 +1,14 @@
+// config.h
+#ifndef ENV_H
+#define ENV_H
+
+
+
+const char* DOL_API_ROOT = "http://172.20.10.8:8080";
+const char* DOL_API_PATH = "/dolibarr/api/index.php";
+const char* DOL_API_KEY = "2PW5SR80mSsohf0cRXn1nR1TsmV0X44j";
+const char* WIFI_SSID = "Delplanque";
+const char* WIFI_PASSWORD = "nicolebg67$$1234++";
+
+
+#endif // ENV_H
